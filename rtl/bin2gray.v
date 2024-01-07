3f22c7f5fbeb0de2e2bedb03e3fd5fe6a4cb572e
Teodor Dimitrov <teodor@endurosat.com>
2024-01-07 16:42:20 +0200

    Rename DATA_WIDTH parameter to WIDTH

--- rtl/bin2gray.v
+++ rtl/bin2gray.v
@@ -4,15 +4,15 @@
 // Module name: bin2gray.v
 //
 // Description: Binary to gray encoder with 
-//              parameterized data width 
+//              parameterized port width 
 //
 //
 
 module bin2gray #(
-        parameter DATA_WIDTH = 8,
+        parameter WIDTH = 8,
     ) (
-        input  [DATA_WIDTH-1 : 0] binary_in,
-        output [DATA)WIDTH-1 : 0] gray_out
+        input  [WIDTH-1 : 0] binary_in,
+        output [WIDTH-1 : 0] gray_out
     );
     
     assign gray_out = (binary_in >> 1) ^ binary_in;
