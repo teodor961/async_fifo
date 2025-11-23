59258058ec11d4271ae8ce7e90dc6078c50ca384
Teodor Dimitrov <teodorpd@gmail.com>
2025-11-23 13:56:29 +0100

    Create tb_pkg.sv for testbench enums and parameters

---
+++ sim/tb/tb_pkg.sv
@@ -0,0 +1,15 @@
+//--------------------------------------------------
+// Created by : Teodor Dimitrov
+// Design     : UVM package
+// Module name: tb_pkg.sv
+//
+// Description: UVM sequence item for the async FIFO module.
+//              extends uvm_sequence_item class
+//
+//
+
+package tb_pkg;
+  
+  typedef enum logic {RANDOM, ALWAYS_ON} ctrl_flow_mode_t;
+
+endpackage
