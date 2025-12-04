bd613427ad99d1d292df5ea28cb2a0f8b6ec1ec0
Teodor Dimitrov <teodorpd@gmail.com>
2025-12-04 19:50:57 +0100

    Save changes to wr_driver

--- sim/tb/async_fifo_wr_driver.sv
+++ sim/tb/async_fifo_wr_driver.sv
@@ -31,26 +31,28 @@ class async_fifo_wr_driver extends async_fifo_driver;
          // Set wr_en initial state
          async_fifo_vif.wr_en <= 0;
          // broadcast the sequence item to anyone connected
+        `uvm_info(m_driver_name, $sformatf("Entered drive_item function"), UVM_DEBUG)
          item_ap.write(m_item);
          for (int i = 0; i < m_item.data_depth; i++) 
            begin
-               `uvm_info(m_driver_name, $sformatf("Wait for full to drop"), UVM_HIGH)
+               `uvm_info(m_driver_name, $sformatf("Wait for full to drop"), UVM_DEBUG)
                if (async_fifo_vif.full == 1)
                  begin
                      async_fifo_vif.wr_en <= 0;
                  end
                wait(async_fifo_vif.full == 0)
-               `uvm_info(m_driver_name, $sformatf("Full FIFO flag dropped"), UVM_HIGH) 
+               `uvm_info(m_driver_name, $sformatf("Full FIFO flag dropped"), UVM_DEBUG) 
                @(posedge async_fifo_vif.wr_clk)
                async_fifo_vif.wr_data <= m_item.data_vector[i];
                
                if (m_item.ctrl_flow_mode == ALWAYS_ON)
-                 `uvm_info(m_driver_name, "Driving as ALWAYS ON", UVM_HIGH)
+                 `uvm_info(m_driver_name, "Driving as ALWAYS ON", UVM_DEBUG)
                else if (m_item.ctrl_flow_mode == RANDOM)
-                 `uvm_info(m_driver_name, "Driving as RANDOM", UVM_HIGH)
+                 `uvm_info(m_driver_name, "Driving as RANDOM", UVM_DEBUG)
                async_fifo_vif.wr_en <= 1;
            end
-         
+         // One clock cycle for the last data byte to be read
+         @(posedge async_fifo_vif.wr_clk)
          async_fifo_vif.wr_en <= 0;
          #200ns;
     endtask
