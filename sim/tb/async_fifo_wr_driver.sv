9d3e3c335d5cf0f0d8b1d9b6cb6883b4ed046758
Teodor Dimitrov <teodor.dimitrov@codasip.com>
2026-01-03 21:57:37 +0100

    async_fifo_wr_driver: Implement random delay insertion for item driving. Clean up debug prints

--- sim/tb/async_fifo_wr_driver.sv
+++ sim/tb/async_fifo_wr_driver.sv
@@ -31,7 +31,6 @@ class async_fifo_wr_driver extends async_fifo_driver;
          // Set wr_en initial state
          async_fifo_vif.wr_en <= 0;
          // broadcast the sequence item to anyone connected
-        `uvm_info(m_driver_name, $sformatf("Entered drive_item function"), UVM_DEBUG)
          item_ap.write(m_item);
          for (int i = 0; i < m_item.data_depth; i++) 
            begin
@@ -46,10 +45,16 @@ class async_fifo_wr_driver extends async_fifo_driver;
                async_fifo_vif.wr_data <= m_item.data_vector[i];
                
                if (m_item.ctrl_flow_mode == ALWAYS_ON)
-                 `uvm_info(m_driver_name, "Driving as ALWAYS ON", UVM_DEBUG)
+                 begin
+                     `uvm_info(m_driver_name, "Driving as ALWAYS ON", UVM_DEBUG)
+                     async_fifo_vif.wr_en <= 1;
+                 end
                else if (m_item.ctrl_flow_mode == RANDOM)
-                 `uvm_info(m_driver_name, "Driving as RANDOM", UVM_DEBUG)
-               async_fifo_vif.wr_en <= 1;
+                 begin
+                     `uvm_info(m_driver_name, "Driving as RANDOM", UVM_DEBUG)
+                     //`uvm_info(m_driver_name, $sformatf("Random delay: %d", $urandom_range(10, 200) ), UVM_HIGH);
+                     #($urandom_range(10, 200)) async_fifo_vif.wr_en <= 1;
+                 end
            end
          // One clock cycle for the last data byte to be read
          @(posedge async_fifo_vif.wr_clk)
